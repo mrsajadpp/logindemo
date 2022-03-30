300e401ed8a63882cd9654df8393af6a6e7ad2f6
Sajad pp <96676224+sajadpp120@users.noreply.github.com>
2022-03-30 20:16:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+const firstName = document.querySelector("#FirstName");
+const lastName = document.querySelector("#LastName");
+const email = document.querySelector("#Email");
+const password = document.querySelector("#Password");
+const loginButton = document.querySelector("#loginButton");
+const text = document.querySelector("#loginText");
+const first = document.querySelector("#firstName");
+const last = document.querySelector("#lastName");
+const mailc = document.querySelector("#mail");
+const photo = document.querySelector("#Photo");
+const dp = document.querySelector("#Prof");
+//Login start
+function login() {
+  if (firstName.value) {
+    localStorage.setItem("First Name",firstName.value);
+    text.innerText = "Welcome " + firstName.value;
+    first.innerText = firstName.value;
+  };
+  if(lastName.value){
+    localStorage.setItem("Last Name",lastName.value);
+    last.innerText = lastName.value;
+  };
+  if(email.value){
+    localStorage.setItem("Email",email.value);
+    mailc.innerText = email.value;
+  };
+  if(password.value){
+    localStorage.setItem("Password",password.value);
+  };
+    if(dp.value){
+    localStorage.setItem("Picture",dp.value);
+    photo.innerHTML = dp.value;
+    console.log(localStorage.getItem("Picture"));
+  };
+  console.log("User " + firstName.value + " Logged");
+  //location.reload();
+};
+//Login end
+if (localStorage.getItem("First Name")) {
+    text.innerText = "Welcome " +  localStorage.getItem("First Name");
+    first.innerText = localStorage.getItem("First Name");
+    
+}else{
+  text.innerText = "Please login"
+};
+
+if (localStorage.getItem("Last Name")) {
+    last.innerText = localStorage.getItem("Last Name");
+};
+if (localStorage.getItem("Email")) {
+    mailc.innerText = localStorage.getItem("Email");
+};
+if (localStorage.getItem("Picture")) {
+    photo.innerHTML = localStorage.getItem("Picture");
+    //console.log(localStorage.getItem("Picture"));
+};
+//Logout start
+function logout(){
+  localStorage.clear();
+  location.reload();
+};
+//Logout end
